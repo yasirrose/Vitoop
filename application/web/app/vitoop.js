5687c05487035930657c4d7e53b80f0230fb720c
pasxel <pas_xel@yahoo.com>
2016-08-25 21:53:00 +0300

    Fix dublicate tinymce

--- application/web/app/vitoop.js
+++ application/web/app/vitoop.js
@@ -243,7 +243,7 @@ app.controller('ToDoController', function ($scope, $http, $filter) {
             $scope.tinymceOptions.height = $scope.user.height_of_todo_list;
             console.log($scope.tinymceOptions);
             $scope.noscrollContainer.maxHeight = 33 * $scope.user.number_of_todo_elements;
-            //$scope.$emit('$tinymce:refresh');
+            $scope.$broadcast('$tinymce:destroy');
             $scope.$broadcast('$tinymce:refresh');
             
             //jQuery('#noscroll-element').css('max-height', 33 * $scope.user.number_of_todo_elements+'px');
