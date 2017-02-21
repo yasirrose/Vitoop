9529e597c888a987a4e8e1dea62170dc81f544c4
pasxel <pas_xel@yahoo.com>
2017-02-21 23:47:44 +0300

    Chnage devops script. Add composer optimization

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -5,6 +5,7 @@ git pull origin master
 
 cd application
 composer install --optimize-autoloader
+composer dump-autoload --optimize --no-dev --classmap-authoritative
 
 php bin/console doc:migr:migr --no-interaction
 php bin/console cache:clear --env=prod
