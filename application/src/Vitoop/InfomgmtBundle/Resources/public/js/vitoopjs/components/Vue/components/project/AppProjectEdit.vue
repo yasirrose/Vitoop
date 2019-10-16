c6e8412d8bdeb43954d15466c4f37434583f20c7
serbin viktor <serbin.work@gmail.com>
2019-10-16 18:32:52 +0300

    feature/#135_rebuild_to_vue ProjectEdit component in progress

--- application/src/Vitoop/InfomgmtBundle/Resources/public/js/vitoopjs/components/Vue/components/project/AppProjectEdit.vue
+++ application/src/Vitoop/InfomgmtBundle/Resources/public/js/vitoopjs/components/Vue/components/project/AppProjectEdit.vue
@@ -1,121 +1,111 @@
 <template>
     <div class="ui-corner-all margin-top-3">
-        <div id="vtp-projectdata-box">
+        <div id="vtp-projectdata-box" v-if="editProject">
 <!--            {% if infoprojectdata is defined and infoprojectdata is not empty %}-->
-            <div class="vtp-uiinfo-info ui-state-highlight ui-corner-all">
-                <span class="vtp-icon ui-icon ui-icon-info"></span>{{ "infoprojectdata" }}
-            </div>
+<!--            <div class="vtp-uiinfo-info ui-state-highlight ui-corner-all">-->
+<!--                <span class="vtp-icon ui-icon ui-icon-info"></span>{{ "infoprojectdata" }}-->
+<!--            </div>-->
 <!--            {% endif %}-->
-            <div v-if="editProject">
-<!--                <input type="text"-->
-<!--                       style="display: none"-->
-<!--                       ng-init="projectId={{ project.getID }}"-->
-<!--                       v-model="projectId">-->
-                <div class="vtp-fh-w60">
-<!--                    <form name="projectSheetForm">-->
-<!--                        <textarea ui-tinymce="tinymceOptions"-->
-<!--                                  v-model="editProject.project_data.sheet"-->
-<!--                                  name="projectText"></textarea>-->
-<!--                    </form>-->
-                    <quill-editor v-model="editProject.project_data.sheet" />
-                </div>
-                <div id="vtp-projectdata-sheet-info-edit"
-                     class="vtp-fh-w25 ui-corner-all"
-                     v-if="isLoaded">
-                    <div class="vtp-fh-w100" style="vertical-align: top; text-align: center; margin-top: 5px">
-                        <div>
-                            <button @click="save"
-                                    class="ui-corner-all ui-state-default"
-                                    :class="{'ui-corner-all ui-state-need-to-save': needToSave}"
-                                    style="padding-bottom: 5px; padding-top: 5px; width: 100%">speichern</button>
+            <div class="vtp-fh-w60" style="flex: 1;margin-right: 10px">
+                <quill-editor v-model="editProject.project_data.sheet"
+                              :options="editorOptions" />
+            </div>
+            <div id="vtp-projectdata-sheet-info-edit"
+                 class="vtp-fh-w25 ui-corner-all"
+                 v-if="isLoaded">
+                <div class="vtp-fh-w100" style="vertical-align: top; text-align: center; margin-top: 5px">
+                    <div>
+                        <button @click="save"
+                                class="ui-corner-all ui-state-default"
+                                :class="{'ui-corner-all ui-state-need-to-save': needToSave}"
+                                style="padding-bottom: 5px; padding-top: 5px; width: 100%">speichern</button>
+                    </div>
+                    <div style="vertical-align: bottom; text-align: left; color: #2779aa; font-size: 14px; padding-top: 10px;">
+                        <div class="vtp-fh-w70">
+                            <input type="checkbox"
+                                   v-model="editProject.project_data.is_private"
+                                   name="projectIsPrivate"
+                                   style="-webkit-appearance: checkbox"/>
+                            <label for="projectIsPrivate" id="sperren-vabel">Projekt sperren</label>
                         </div>
-                        <div style="vertical-align: bottom; text-align: left; color: #2779aa; font-size: 14px; padding-top: 10px;">
-                            <div class="vtp-fh-w70">
-                                <input type="checkbox"
-                                       v-model="editProject.project_data.is_private"
-                                       name="projectIsPrivate"
-                                       style="-webkit-appearance: checkbox"/>
-                                <label for="projectIsPrivate" id="sperren-vabel">Projekt sperren</label>
-                            </div>
-                            <div class="vtp-fh-w70">
-                                <input type="checkbox"
-                                       v-model="editProject.project_data.is_for_related_users"
-                                       name="projectForRelated" style="-webkit-appearance: checkbox"/>
-                                <label for="projectForRelated" id="verstecken-vabel">Projekt verstecken</label>
-                            </div>
+                        <div class="vtp-fh-w70">
+                            <input type="checkbox"
+                                   v-model="editProject.project_data.is_for_related_users"
+                                   name="projectForRelated" style="-webkit-appearance: checkbox"/>
+                            <label for="projectForRelated" id="verstecken-vabel">Projekt verstecken</label>
                         </div>
                     </div>
+                </div>
 <!--                    {% if (showasprojectowner is not null) and (showasprojectowner) %}-->
-                        <div class="vtp-fh-w100 vtp-ui-corner-all blau"
-                             v-if="editProject.project_data.rel_users.length > 0"
-                             style="vertical-align: top; margin-top: 20px; font-size: 14px">
-                            <div class="prj-edit-header">
-                                <div class="vtp-fh-w60">
-                                    <span><strong>Benutzer</strong></span>
-                                </div>
-                                <div class="vtp-fh-w35">
-                                    <span><strong>Rechte</strong></span>
-                                </div>
+                    <div class="vtp-fh-w100 vtp-ui-corner-all blau"
+                         v-if="editProject.project_data.rel_users.length > 0"
+                         style="vertical-align: top; margin-top: 20px; font-size: 14px">
+                        <div class="prj-edit-header">
+                            <div class="vtp-fh-w60">
+                                <span><strong>Benutzer</strong></span>
                             </div>
+                            <div class="vtp-fh-w35">
+                                <span><strong>Rechte</strong></span>
+                            </div>
+                        </div>
 <!--                            {% verbatim %}-->
-                                <div v-for="rel in editProject.project_data.rel_users">
-                                    <div class="vtp-fh-w60">
-                                        <label :for="`userReadOnly-${rel.user.id}`"
-                                               style="margin-right: 20px">{{ rel.user.username }}</label>
-                                    </div>
-                                    <div class="vtp-fh-w35" style="text-align: left">
-                                        <input type="checkbox"
-                                               v-model="rel.read_only"
-                                               :name="`userReadOnly-${rel.user.id}`"
-                                               class="valid-checkbox" />
-                                    </div>
+                            <div v-for="rel in editProject.project_data.rel_users">
+                                <div class="vtp-fh-w60">
+                                    <label :for="`userReadOnly-${rel.user.id}`"
+                                           style="margin-right: 20px">{{ rel.user.username }}</label>
                                 </div>
+                                <div class="vtp-fh-w35" style="text-align: left">
+                                    <input type="checkbox"
+                                           v-model="rel.read_only"
+                                           :name="`userReadOnly-${rel.user.id}`"
+                                           class="valid-checkbox" />
+                                </div>
+                            </div>
 <!--                            {% endverbatim %}-->
-                        </div>
+                    </div>
 <!--                    {%endif%}-->
 <!--                    {% if (showasprojectowner is not null) and (showasprojectowner) %}-->
-                    <div class="vtp-fh-w100" style="vertical-align: top; margin-top: 20px; color: #2779aa; font-size: 14px">
-                        <div class="vtp-fh-w100" style="vertical-align: top; margin-bottom: 5px">
-                            <label for="newUser"><strong>Neuer Benutzer:</strong></label>
-                        </div>
-                        <div class="vtp-fh-w100 vtp-new-user-search">
-                            <v-select :options="options"
-                                      label="username"
-                                      @input="selectUser"
-                                      @search="searchUser"/>
-                            <button @click="addUser()"
-                                    class="ui-corner-all vtp-fh-w60 ui-state-default vtp-button">verknüpfen</button>
-                            <button @click="removeUser()"
-                                    class="ui-corner-all vtp-fh-w40 ui-state-default vtp-button" style="float: right">löschen</button>
-                        </div>
-                        <span v-if="isError"
-                              id="error-span"
-                              class="form-error">
-                                Vitoooops!: <span ng-bind="message"></span>
-                            </span>
-                        <div class="vtp-uiinfo-info ui-state-highlight ui-corner-all"
-                             v-if="isSuccess" style="transition: 0s linear all;">
-                            <span class="vtp-icon ui-icon ui-icon-info"></span>
-                            <span ng-bind="message"></span>
-                        </div>
+                <div class="vtp-fh-w100" style="vertical-align: top; margin-top: 20px; color: #2779aa; font-size: 14px">
+                    <div class="vtp-fh-w100" style="vertical-align: top; margin-bottom: 5px">
+                        <label for="newUser"><strong>Neuer Benutzer:</strong></label>
                     </div>
-                    <div class="vtp-fh-w100" v-if="isOwner">
-                        <p v-if="isDeleting">
-                            Dieser Vorgang kann nicht rückgängig gemacht werden. Soll das Projekt wirklich gelöscht werden?
-                        </p>
-                        <button @click="isDeleting = true"
-                                v-if="!isDeleting"
-                                class="ui-corner-all vtp-button-light" style="margin-top: 20px; width: 100%">Projekt löschen</button>
-                        <button @click="remove"
-                                v-if="isDeleting"
-                                class="ui-corner-all vtp-fh-w30 vtp-button-light">Ja</button>
-                        <button @click="isDeleting = false"
-                                v-if="isDeleting"
-                                class="ui-corner-all vtp-fh-w30 vtp-button-light">Nein</button>
+                    <div class="vtp-fh-w100 vtp-new-user-search">
+                        <v-select :options="options"
+                                  label="username"
+                                  @input="selectUser"
+                                  @search="searchUser"/>
+                        <button @click="addUser()"
+                                class="ui-corner-all vtp-fh-w60 ui-state-default vtp-button">verknüpfen</button>
+                        <button @click="removeUser()"
+                                class="ui-corner-all vtp-fh-w40 ui-state-default vtp-button" style="float: right">löschen</button>
                     </div>
-                    <div class="vtp-fh-w100 vtp-new-user-info"></div>
-<!--                    {%endif%}-->
+                    <span v-if="isError"
+                          id="error-span"
+                          class="form-error">
+                            Vitoooops!: <span ng-bind="message"></span>
+                        </span>
+                    <div class="vtp-uiinfo-info ui-state-highlight ui-corner-all"
+                         v-if="isSuccess" style="transition: 0s linear all;">
+                        <span class="vtp-icon ui-icon ui-icon-info"></span>
+                        <span>{{ message }}</span>
+                    </div>
                 </div>
+                <div class="vtp-fh-w100" v-if="isOwner">
+                    <p v-if="isDeleting">
+                        Dieser Vorgang kann nicht rückgängig gemacht werden. Soll das Projekt wirklich gelöscht werden?
+                    </p>
+                    <button @click="isDeleting = true"
+                            v-if="!isDeleting"
+                            class="ui-corner-all vtp-button-light" style="margin-top: 20px; width: 100%">Projekt löschen</button>
+                    <button @click="remove"
+                            v-if="isDeleting"
+                            class="ui-corner-all vtp-fh-w30 vtp-button-light">Ja</button>
+                    <button @click="isDeleting = false"
+                            v-if="isDeleting"
+                            class="ui-corner-all vtp-fh-w30 vtp-button-light">Nein</button>
+                </div>
+                <div class="vtp-fh-w100 vtp-new-user-info"></div>
+<!--                    {%endif%}-->
             </div>
         </div>
     </div>
@@ -130,6 +120,7 @@
         components: {vSelect},
         data() {
             return {
+                message: null,
                 isError: false,
                 isSuccess: false,
                 isOwner: false,
@@ -140,7 +131,23 @@
 
                 options: [],
                 user: null,
-                needToSave: false
+                needToSave: false,
+
+                editorOptions: {
+                    modules: {
+                        toolbar: [
+                            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+                            ['bold', 'italic', 'underline', 'blockquote', 'strike', { 'script': 'super' }, { 'script': 'sub' }, 'code-block'],
+                            [{ 'align': [] }],
+                            [{ 'indent': '-1' }, { 'indent': '+1' }],
+                            ['link', 'image', 'video'],
+                            [{ 'color': [] }, { 'background': [] }]
+                        ],
+                        syntax: {
+                            highlight: text => hljs.highlightAuto(text).value
+                        }
+                    }
+                }
             }
         },
         watch: {
@@ -205,7 +212,7 @@
                     .catch(err => console.dir(err));
             },
             removeUser() {
-                axios.delete(`/api/project/${this.project.id}/user/${this.user.originalObject.id}`)
+                axios.delete(`/api/project/${this.project.id}/user/${this.user.id}`)
                     .then(response => {
                         console.log(response)
                     })
@@ -216,12 +223,16 @@
 </script>
 
 <style scoped lang="scss">
+    #vtp-projectdata-box {
+        display: flex;
+    }
+
     #vtp-projectdata-sheet-info-edit {
         overflow: visible;
     }
 
     .dropdown {
-        margin-bottom: 7px;
+        margin-bottom: 5px;
     }
 
     .vtp-new-user-search {
@@ -230,6 +241,8 @@
 
             .dropdown-toggle {
                 background: white;
+                height: 28px;
+                padding: 1px 0 4px;
             }
 
             .form-control {
