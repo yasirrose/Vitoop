53ec30e2591aaeec0602d0beb95c990635f41a9d
serbin viktor <serbin.work@gmail.com>
2019-10-18 12:49:25 +0300

    feature/#135_rebuild_to_vue Table component in progress

--- application/src/Vitoop/InfomgmtBundle/Resources/public/js/vitoopjs/components/Vue/components/tables/heads/BookTableHead.vue
+++ application/src/Vitoop/InfomgmtBundle/Resources/public/js/vitoopjs/components/Vue/components/tables/heads/BookTableHead.vue
@@ -3,13 +3,13 @@
         <th style="width:96px;"><span class="table-head">{{ dateTitle }}</span></th>
         <th style="width:20px"></th>
         <th><span class="table-head">Titel</span></th>
-        <th style="width:15%;"><span class="table-head">Autor</span></th>
+        <th style="width:21%;"><span class="table-head">Autor</span></th>
         <th style="width:63px;"><span class="table-head">Seiten</span></th>
         <th style="width:96px;" data-b-searchable="false"><span class="table-head">Bewertung</span></th>
         <th style="width:42px;" data-b-searchable="false"><span class="table-head">RR</span></th>
-        <th style="width:12%;"><span class="table-head">von</span></th>
-        <th style="width:8%;" data-b-searchable="false" v-if="$store.state.admin !== null"><span class="table-head">Stored</span></th>
-        <th style="width: 54px"
+        <th style="width:14%;"><span class="table-head">von</span></th>
+        <th style="width:54px;"
+            v-if=""
             data-b-sortable="false"
             data-b-searchable="false">
             <span class="table-head">{{ linkTitle }}</span>
@@ -19,7 +19,7 @@
 
 <script>
     export default {
-        name: "PdfTableHead",
+        name: "BookTableHead",
         props: ['dateTitle','linkTitle']
     }
 </script>
