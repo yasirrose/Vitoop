bcf187ba10020c14fdb0cae36ba006d9eca07b87
serbin viktor <serbin.work@gmail.com>
2019-12-02 09:49:19 +0200

    feature/#146 tags without reloading page implemented

--- application/src/Vitoop/InfomgmtBundle/Resources/public/js/vitoopjs/components/Vue/components/Tags.vue
+++ application/src/Vitoop/InfomgmtBundle/Resources/public/js/vitoopjs/components/Vue/components/Tags.vue
@@ -2,32 +2,50 @@
     <div id="vtp-res-tags" class="scrollbar-inner">
         <table id="tags-list" class="dataTable table-datatables noscroll-container">
             <thead>
-            <tr style="">
-                <th style="width: 29%" class="ui-state-default">Tag:</th>
-                <th class="ui-state-default">X</th>
-                <th class="ui-state-default">Projekt:</th>
-                <th class="ui-state-default">Lexicon:</th>
-                <th class="ui-state-default">Pdf:</th>
-                <th class="ui-state-default">Textlink:</th>
-                <th class="ui-state-default">Link:</th>
-                <th class="ui-state-default">Buch:</th>
-                <th class="ui-state-default">Adresse:</th>
+            <tr>
+                <td style="padding: 0 15px 0 0;">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th style="width: 29%" class="ui-state-default">Tag:</th>
+                                <th class="ui-state-default">X</th>
+                                <th class="ui-state-default">Projekt:</th>
+                                <th class="ui-state-default">Lexicon:</th>
+                                <th class="ui-state-default">Pdf:</th>
+                                <th class="ui-state-default">Textlink:</th>
+                                <th class="ui-state-default">Link:</th>
+                                <th class="ui-state-default">Buch:</th>
+                                <th class="ui-state-default">Adresse:</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </td>
             </tr>
             </thead>
-            <tbody>
-                <tr class="ui-corner-all"
-                    :class="{even: index % 2 === 0}"
-                    v-for="(tag,index) in tags"
-                    :key="tag.id">
-                    <td>{{ tag.text }}:</td>
-                    <td>{{ tag.cnt }}</td>
-                    <td>{{ tag.prjc }}</td>
-                    <td>{{ tag.lexc }}</td>
-                    <td>{{ tag.pdfc }}</td>
-                    <td>{{ tag.telic }}</td>
-                    <td>{{ tag.linkc }}</td>
-                    <td>{{ tag.bookc }}</td>
-                    <td>{{ tag.adrc }}</td>
+            <tbody v-if="tags.length > 0">
+                <tr>
+                    <td style="padding: 0 0;">
+                        <div class="body-scroll">
+                            <table>
+                                <tbody>
+                                    <tr class="ui-corner-all"
+                                        :class="{even: index % 2 === 0}"
+                                        v-for="(tag,index) in tags"
+                                        :key="tag.id">
+                                        <td>{{ tag.text }}:</td>
+                                        <td>{{ tag.cnt }}</td>
+                                        <td>{{ tag.prjc }}</td>
+                                        <td>{{ tag.lexc }}</td>
+                                        <td>{{ tag.pdfc }}</td>
+                                        <td>{{ tag.telic }}</td>
+                                        <td>{{ tag.linkc }}</td>
+                                        <td>{{ tag.bookc }}</td>
+                                        <td>{{ tag.adrc }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -37,16 +55,23 @@
 <script>
     export default {
         name: "Tags",
-        inject: ['tags'],
+        data() {
+            return {
+                tags: []
+            }
+        },
         mounted() {
-            $('#tags-list').scrolltable({
-                stripe: true,
-                maxHeight: 350
-            });
+            axios('/api/v1/tags')
+                .then(({data}) => {
+                    this.tags = data;
+                })
         }
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    .body-scroll {
+        height: 350px;
+        overflow: auto;
+    }
 </style>
