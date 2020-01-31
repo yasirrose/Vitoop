12acdf83310fca88ec778194f90cbf8ba169778d
Eugene Kirilov <kogomar@gmail.com>
2020-01-31 10:55:03 +0200

    Added methods for Add and Remove Relations User in Conversation

--- application/src/Vitoop/InfomgmtBundle/Controller/V1/ConversationController.php
+++ application/src/Vitoop/InfomgmtBundle/Controller/V1/ConversationController.php
@@ -15,8 +15,11 @@ use Vitoop\InfomgmtBundle\Entity\Conversation;
 use Vitoop\InfomgmtBundle\Entity\ConversationMessage;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Vitoop\InfomgmtBundle\Entity\RelConversationUser;
+use Vitoop\InfomgmtBundle\Entity\User;
 use Vitoop\InfomgmtBundle\Repository\ConversationMessageRepository;
 use Vitoop\InfomgmtBundle\Repository\ConversationRepository;
+use Vitoop\InfomgmtBundle\Repository\RelConversationUserRepository;
 use Vitoop\InfomgmtBundle\Repository\UserRepository;
 use Vitoop\InfomgmtBundle\Service\MessageService;
 use Vitoop\InfomgmtBundle\Service\VitoopSecurity;
@@ -90,6 +93,73 @@ class ConversationController extends ApiController
         return $this->getApiResponse(['success' => 'success']);
     }
 
+    /**
+     * @Route("/user", methods={"POST"} , requirements={"id": "\d+"})
+     * @param VitoopSecurity $vitoopSecurity
+     * @param Conversation $conversation
+     * @param Request $request
+     * @param UserRepository $userRepository
+     * @param RelConversationUserRepository $conversationUserRepository
+     * @return object
+     */
+    public function addUserToConversation(
+        VitoopSecurity $vitoopSecurity,
+        Conversation $conversation,
+        Request $request,
+        UserRepository $userRepository,
+        RelConversationUserRepository $conversationUserRepository
+    )
+    {
+        $currentUser = $vitoopSecurity->getUser();
+        $this->checkAccessForRelUserAction($conversation, $vitoopSecurity);
+        $response = null;
+
+        $user = $userRepository->find($request->get('userId'));
+        if (is_null($user)) {
+            $response = ['status' => 'error', 'message' => 'User is not found'];
+        } elseif ($user->getUsername() == $currentUser->getUsername()) {
+            $response = ['status' => 'error', 'message' => 'User is equal to current'];
+        } else {
+            foreach ($conversation->getConversationData()->getRelUsers() as $relUser) {
+                if ($user->getUsername() == $relUser->getUser()->getUsername()) {
+                    $response = ['status' => 'error', 'message' => 'User is already added'];
+                    break;
+                }
+            }
+        }
+        if (is_null($response)) {
+            $rcu = new RelConversationUser($conversation->getConversationData(), $user);
+            $conversationUserRepository->addUser($rcu);
+
+            $response = $rcu;
+        }
+
+        return $this->getApiResponse($response);
+    }
+
+    /**
+     * @Route("/user/{userID}", methods={"DELETE"})
+     * @param VitoopSecurity $vitoopSecurity
+     * @param Conversation $conversation
+     * @param User $user
+     * @param RelConversationUserRepository $conversationUserRepository
+     * @ParamConverter("user", class="Vitoop\InfomgmtBundle\Entity\User", options={"id" = "userID"})
+     * @return object
+     */
+    public function removeUserFromConversation(
+        VitoopSecurity $vitoopSecurity,
+        Conversation $conversation,
+        User $user,
+        RelConversationUserRepository $conversationUserRepository
+    )
+    {
+        $this->checkAccessForRelUserAction($conversation, $vitoopSecurity);
+        $relConversationUser = $conversationUserRepository->getRel($user, $conversation);
+        $conversationUserRepository->removeUser($relConversationUser);
+
+        return $this->getApiResponse($relConversationUser);
+    }
+
     private function checkAccess(Conversation $conversation, VitoopSecurity $vitoopSecurity)
     {
         if (!$conversation->getConversationData()->availableForReading($vitoopSecurity->getUser())) {
@@ -100,6 +170,13 @@ class ConversationController extends ApiController
     private function checkAccessForDelete(ConversationMessage $message, VitoopSecurity $vitoopSecurity)
     {
         if (!$message->availableForDelete($vitoopSecurity->getUser())) {
+            throw new AccessDeniedHttpException;
+        }
+    }
+
+    private function checkAccessForRelUserAction(Conversation $conversation, VitoopSecurity $vitoopSecurity)
+    {
+        if (!$conversation->getConversationData()->availableForRelUserAction($vitoopSecurity->getUser())) {
             throw new AccessDeniedHttpException;
         }
     }
