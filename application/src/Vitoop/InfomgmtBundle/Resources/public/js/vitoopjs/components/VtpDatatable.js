ea677015158252d6ab33ddb10adceb029474d74c
v-serbin <viktor.serbin88@gmail.com>
2020-03-19 16:41:19 +0200

    issue/#182_list_sorting addition changes

--- application/src/Vitoop/InfomgmtBundle/Resources/public/js/vitoopjs/components/VtpDatatable.js
+++ application/src/Vitoop/InfomgmtBundle/Resources/public/js/vitoopjs/components/VtpDatatable.js
@@ -365,17 +365,10 @@ export default class VtpDatatable {
     }
 
     getDefaultOrder() {
-        if (vitoopState.state.secondSearch.dateFrom !== '') {
+        if (vitoopState.state.secondSearch.dateFrom !== '' && /pdf|teli/.test(this.resType)) {
             return [0, 'asc'];
         } else {
             return [0, 'desc'];
-        }
-
-        if (this.resType == 'pdf' || this.resType == 'teli') {
-            let dateRangeFilter = vitoopApp.secondSearch.dateRange;
-            if (!dateRangeFilter.isEmpty()) {
-                return [0, 'desc'];
-            }
         }
 
         if (!($('#vtp-lexicondata-title').length)) {
