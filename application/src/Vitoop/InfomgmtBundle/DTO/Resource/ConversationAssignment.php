14a35a01c7528eafb2df21159d5f6d529b0d92c1
Aleksey Plotnikov <alexei.plotnikoff@gmail.com>
2020-06-09 11:52:26 +0300

    Issue #206. Add possibility to add records to conversations

---
+++ application/src/Vitoop/InfomgmtBundle/DTO/Resource/ConversationAssignment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Vitoop\InfomgmtBundle\DTO\Resource;
+
+/**
+ * Class ConversationAssignment
+ * @package Vitoop\InfomgmtBundle\DTO\Resource
+ */
+class ConversationAssignment extends ProjectAssignment
+{
+}
