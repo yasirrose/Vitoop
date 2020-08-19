35a354a15082ecca7c9a4da142b5e57683839a78
v-serbin <viktor.serbin88@gmail.com>
2020-08-19 14:58:46 +0300

    feature/#219 dataP fixed

--- application/src/Vitoop/InfomgmtBundle/Resources/public/js/vitoopjs/components/Vue/components/user/UserDataP.vue
+++ application/src/Vitoop/InfomgmtBundle/Resources/public/js/vitoopjs/components/Vue/components/user/UserDataP.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="vtp-content">
         <fieldset class="ui-corner-all margin-top-3">
-            <div id="vtp-terms-user" class="ui-corner-all" style="padding: 30px">
-                <div v-html="dataP"></div>
+            <div id="vtp-terms-user"
+                 class="ui-corner-all">
+                <div v-if="dataP" v-html="dataP"></div>
+                <div v-else class="preloader">
+                    <img src="/img/loader.gif" />
+                </div>
             </div>
         </fieldset>
     </div>
@@ -11,12 +15,41 @@
 <script>
     export default {
         name: "UserDataP",
-        inject: ['dataP'],
+        data() {
+            return {
+                dataP: null,
+            }
+        },
+        created() {
+            axios('/api/v1/users/datap')
+                .then(({ data: { datap } }) => this.dataP = datap)
+                .catch(err => console.dir(err));
+        }
     }
 </script>
 
 <style>
     #gtx-trans {
         display: none;
+    }
+
+    #vtp-terms-user {
+        padding: 30px;
+        position: relative;
+    }
+</style>
+
+<style scoped lang="scss">
+    .preloader {
+        height: 100px;
+        display: table-row;
+
+        img {
+            width: 40px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
     }
 </style>
