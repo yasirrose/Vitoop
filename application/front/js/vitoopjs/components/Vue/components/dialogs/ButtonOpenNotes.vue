d919596fbb04dd73c59e50e11fd6e160df9a3f5a
v-serbin <viktor.serbin88@gmail.com>
2020-11-15 20:48:07 +0200

    feature/#247_small_65 another icon variant added but commented

--- application/front/js/vitoopjs/components/Vue/components/dialogs/ButtonOpenNotes.vue
+++ application/front/js/vitoopjs/components/Vue/components/dialogs/ButtonOpenNotes.vue
@@ -2,7 +2,9 @@
     <button @click="openNotes"
             class="ui-state-default ui-corner-all vtp-button"
             style="width: 34px">
-        <i class="fa fa-sticky-note" style="font-size: 12px"></i>
+<!--        <i class="fa fa-sticky-note" style="font-size: 12px"></i>-->
+        <span class="ui-icon ui-icon-document-b"></span>
+<!--        <i class="far fa-file" style="font-size: 12px"></i>-->
     </button>
 </template>
 
