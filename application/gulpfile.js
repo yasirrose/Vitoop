7b42c0f64d6aafe62ca7856bfddb9cbd7473e184
v-serbin <viktor.serbin88@gmail.com>
2020-11-30 20:48:24 +0200

    issue/#247_small_65 done

--- application/gulpfile.js
+++ application/gulpfile.js
@@ -91,7 +91,7 @@ gulp.task('vitoop-app', function () {
         'front/js/vitoopjs/app/boot.js'
         ])
         .pipe(webpack({
-            mode: 'production',
+            mode: 'development',
             output: {
                 publicPath: "/js/",
                 filename: 'vitoop-app.js'
@@ -211,7 +211,7 @@ gulp.task('img', function() {
 gulp.task('pdf', function () {
     return gulp.src('front/js/pdf.editor/pdf.editor.js')
         .pipe(webpack({
-            mode: 'production',
+            mode: 'development',
             output: {
                 publicPath: "/build/",
                 filename: 'pdf.editor.js'
@@ -231,7 +231,7 @@ gulp.task('pdf', function () {
 gulp.task('default', gulp.series(['img', 'tinymce-scss', 'scss', 'js', 'pdf', 'pdf-view-js']));
 
 gulp.task('set-prod', function() {
-    return env = 'prod';
+    // return env = 'prod';
 });
 
 gulp.task('prod', gulp.series(['set-prod', 'default']));
