bc52d7b35dfe6e864abb37b8b45c82587904541e
v-serbin <viktor.serbin88@gmail.com>
2020-12-06 18:35:52 +0200

    issue/#252 gulpfile recovered

--- application/gulpfile.js
+++ application/gulpfile.js
@@ -91,7 +91,7 @@ gulp.task('vitoop-app', function () {
         'front/js/vitoopjs/app/boot.js'
         ])
         .pipe(webpack({
-            mode: 'development',
+            mode: 'production',
             output: {
                 publicPath: "/js/",
                 filename: 'vitoop-app.js'
@@ -212,7 +212,7 @@ gulp.task('img', function() {
 gulp.task('pdf', function () {
     return gulp.src('front/js/pdf.editor/pdf.editor.js')
         .pipe(webpack({
-            mode: 'development',
+            mode: 'production',
             output: {
                 publicPath: "/build/",
                 filename: 'pdf.editor.js'
@@ -232,7 +232,7 @@ gulp.task('pdf', function () {
 gulp.task('default', gulp.series(['img', 'tinymce-scss', 'scss', 'js', 'pdf', 'pdf-view-js']));
 
 gulp.task('set-prod', function() {
-    // return env = 'prod';
+    return env = 'prod';
 });
 
 gulp.task('prod', gulp.series(['set-prod', 'default']));
