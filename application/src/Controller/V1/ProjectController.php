cb0f9577d0fd753af287c3bf6b884dc6f06e7b49
Harsh Sanghani <harsh.angelinfotech@gmail.com>
2023-04-05 10:12:53 +0530

    Issue #343 Master-update close button and open project list logout issue.

--- application/src/Controller/V1/ProjectController.php
+++ application/src/Controller/V1/ProjectController.php
@@ -108,7 +108,7 @@ class ProjectController extends ApiController
         $search = SearchResource::createFromRequest($request, $this->getUser(), $project->getId());
 
         $resourceRepository = $resourceManager->getRepository($resType);
-        $resources = $resourceRepository->getResourcesWithDividers($search);
+        $resources = $resourceRepository->getResources($search);
         $total = $resourceRepository->getResourcesTotal($search);
 
         if ('prj' === $resType) {
